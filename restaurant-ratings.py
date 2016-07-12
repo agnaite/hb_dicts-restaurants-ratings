343a2e7d05ff087edcaa617a1d98e063744a4285
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 17:08:18 -0700

    added comments

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -20,24 +20,28 @@ the_file.close()
 name = raw_input("Hi, what is your name?")
 print "Hello %s!" % (name)
 
+# asks user to enter new restaurant ratings and updates dictionary until user enters 'q'
+
 while True:
-
+    #generates random number from 0 to length of dictionary - 1
     random_number = random.randint(0, len(restaurant_ratings)-1)
+    #picks a random key
     random_restaurant = restaurant_ratings.keys()[random_number]
-
+    #prompts user for new rating after displaying the current one
     print "{}'s current rating is: {}".format(random_restaurant, restaurant_ratings[random_restaurant])
     user_rating = raw_input("Enter new rating: ")
-
+    #breaks out of loop if user enters 'q'
     if user_rating == 'q':
         break
 
+    #updates the dictionary
     restaurant_ratings[random_restaurant] = user_rating
-
 
 #this turns the dictionary into a list of tuples and sorts it and prints the restaurant and ratings
 
 restaurant_ratings = restaurant_ratings.items()
 restaurant_ratings.sort()
 
+#prints list of restaurants and ratings
 for restaurant, rating in restaurant_ratings:
     print '{} is rated {}'.format(restaurant, rating)
